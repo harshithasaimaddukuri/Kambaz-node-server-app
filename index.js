a330f6b555b68e11a7c5b88ce3dcdbdd598333df
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-11-15 01:33:17 -0500

    Fixed index

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
 import session from "express-session";
-import db from "./Kambaz/Database/index.js";
+
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import PathParameters from "./Lab5/PathParameters.js";
+import QueryParameters from "./Lab5/QueryParameters.js";
+import WorkingWithObjects from "./Lab5/WorkingWithObjects.js";
+import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
-import ModuleRoutes from "./Kambaz/Modules/routes.js";
+import ModulesRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
-import Lab5 from "./Lab5/index.js";
+
+import db from "./Kambaz/Database/index.js";
 
 const app = express();
 
 app.use(
   cors({
     credentials: true,
-    origin: function (origin, callback) {
-      if (!origin) {
-        return callback(null, true);
-      }
-      
-      return callback(null, true);
-    },
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
   })
 );
+
+app.set("trust proxy", 1);
 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
@@ -31,20 +34,34 @@ const sessionOptions = {
   saveUninitialized: false,
 };
 
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+    domain: "kambaz-node-server-app-vy3q.onrender.com",
+    partitioned: true,
+  };
+}
+
 app.use(session(sessionOptions));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Welcome to Full Stack Development!");
-});
 
 UserRoutes(app, db);
 CourseRoutes(app, db);
-ModuleRoutes(app, db);
+ModulesRoutes(app, db);
 AssignmentRoutes(app, db);
 EnrollmentRoutes(app, db);
 Lab5(app);
+Hello(app);
+PathParameters(app);
+QueryParameters(app);
+WorkingWithObjects(app);
+WorkingWithArrays(app);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
