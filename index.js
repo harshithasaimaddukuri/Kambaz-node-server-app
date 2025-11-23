ee4cc52c3528ce6964ae56e67f4930509dd18c29
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-11-22 23:58:52 -0500

    session

--- index.js
+++ index.js
@@ -1,59 +1,48 @@
 import express from 'express'
 import Hello from './Hello.js'
 import Lab5 from './Lab5/index.js'
-import db from "./Kambaz/Database/index.js";
-import UserRoutes from "./Kambaz/Users/routes.js";
-import CourseRoutes from "./Kambaz/Courses/routes.js";
-import ModulesRoutes from "./Kambaz/Modules/routes.js";
-import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
-import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
+import db from "./kambaz/Database/index.js";
+import UserRoutes from "./kambaz/Users/routes.js";
+import CourseRoutes from "./kambaz/Courses/routes.js";
+import ModulesRoutes from "./kambaz/Modules/routes.js";
+import AssignmentsRoutes from "./kambaz/Assignments/routes.js";
+import EnrollmentsRoutes from "./kambaz/Enrollments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
-
 const app = express();
-
-const clientUrl = process.env.CLIENT_URL 
-  ? process.env.CLIENT_URL.trim().replace(/['"]/g, '') 
-  : "http://localhost:3000";
-
-console.log("CLIENT_URL:", clientUrl); 
-
 app.use(
   cors({
     credentials: true,
-    origin: clientUrl,
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
   })
 );
-
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: process.env.SERVER_ENV !== "development", // true in production
-    sameSite: process.env.SERVER_ENV !== "development" ? "none" : "lax", // "none" for cross-site
-    maxAge: 1000 * 60 * 60 * 24,
-  }
 };
-
 if (process.env.SERVER_ENV !== "development") {
   sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+  };
 }
-
 app.use(session(sessionOptions));
 app.use(express.json());
 
 UserRoutes(app, db);
+
 CourseRoutes(app, db);
+
 ModulesRoutes(app, db);
+
 AssignmentsRoutes(app, db);
+
 EnrollmentsRoutes(app, db);
+
 Lab5(app)
 Hello(app)
-
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
-  console.log("CORS origin:", clientUrl);
-});
+app.listen(4000)
