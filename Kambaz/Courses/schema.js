efe3a036b404bdfc46edfcea3aee12c9a84eafb4
harshithasaimaddukuri <maddukuri.h@northeastern.edu>
2025-12-04 23:28:56 -0500

    Add models and schemas

---
+++ Kambaz/Courses/schema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+  {
+    _id: String,
+    name: String,
+    number: String,
+    credits: Number,
+    description: String,
+    startDate: String,
+    endDate: String,
+    department: String,
+    image: String,
+  },
+  { collection: "courses" }
+);
+
+export default courseSchema;
